5de76fcc13ba25582997f6d4c847cf93b535930a
Ivan <lanuevo68@gmail.com>
2026-01-07 12:13:43 +0800

    add date picker to select expense date and display formatted date

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -14,6 +14,7 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
 
   bool get _isFormValid {
     return _titleController.text.trim().isNotEmpty &&
@@ -39,12 +40,30 @@ class _NewExpenseState extends State<NewExpense> {
     super.dispose();
   }
 
+  void _pickDate() async {
+    final now = DateTime.now();
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime(now.year - 5),
+      lastDate: DateTime(now.year + 5),
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
+  }
+
   void _saveExpense() {
     final title = _titleController.text.trim();
     final amount = double.parse(_amountController.text);
+    final formattedDate = DateFormat.yMMMd().format(_selectedDate);
 
     print('Title: $title');
     print('Amount: \$${amount.toStringAsFixed(2)}');
+    print('Date: $formattedDate');
 
     // Navigator.pop(context);
   }
@@ -64,9 +83,7 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           TextField(
             controller: _amountController,
-            keyboardType: const TextInputType.numberWithOptions(
-              decimal: true,
-            ),
+            keyboardType: const TextInputType.numberWithOptions(decimal: true),
             inputFormatters: [
               FilteringTextInputFormatter.allow(
                 RegExp(r'^\d*\.?\d{0,2}'),
@@ -76,6 +93,16 @@ class _NewExpenseState extends State<NewExpense> {
               labelText: 'Amount',
               prefixText: '\$ ',
             ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: 'Date',
+              hintText: DateFormat.yMMMd().format(_selectedDate),
+              suffixIcon: const Icon(Icons.calendar_today),
+            ),
+            onTap: _pickDate,
           ),
           const SizedBox(height: 16),
           Row(
