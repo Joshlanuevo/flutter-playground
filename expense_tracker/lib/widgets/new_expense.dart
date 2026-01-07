9b4e7a12260a38bff7767f1e47a6ac03fdb0a486
Ivan <lanuevo68@gmail.com>
2026-01-07 12:07:33 +0800

    add amount input with validation and disable save when form is incomplete

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/services.dart';
+
 class NewExpense extends StatefulWidget {
-  const NewExpense({ super.key });
+  const NewExpense({super.key});
 
   @override
   State<NewExpense> createState() {
@@ -11,7 +13,42 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
-  
+  final _amountController = TextEditingController();
+
+  bool get _isFormValid {
+    return _titleController.text.trim().isNotEmpty &&
+        double.tryParse(_amountController.text) != null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _titleController.addListener(_onInputChanged);
+    _amountController.addListener(_onInputChanged);
+  }
+
+  void _onInputChanged() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  void _saveExpense() {
+    final title = _titleController.text.trim();
+    final amount = double.parse(_amountController.text);
+
+    print('Title: $title');
+    print('Amount: \$${amount.toStringAsFixed(2)}');
+
+    // Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -25,14 +62,33 @@ class _NewExpenseState extends State<NewExpense> {
               labelText: 'Title',
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: const TextInputType.numberWithOptions(
+              decimal: true,
+            ),
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(
+                RegExp(r'^\d*\.?\d{0,2}'),
+              ),
+            ],
+            decoration: const InputDecoration(
+              labelText: 'Amount',
+              prefixText: '\$ ',
+            ),
+          ),
+          const SizedBox(height: 16),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              ElevatedButton(
+              TextButton(
                 onPressed: () {
-                  print(_titleController.text);
-                  // Navigator.pop(context);
+                  Navigator.pop(context);
                 },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: _isFormValid ? _saveExpense : null,
                 child: const Text('Save Expense'),
               ),
             ],
