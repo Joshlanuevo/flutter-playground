da21748c5e0ed8a0590220c1254b25bb6833425a
Ivan <lanuevo68@gmail.com>
2026-01-13 13:30:51 +0800

    extract color scheme to a variable for improved readability and maintainability. also, use it  for setting themes

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -2,16 +2,38 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 241, 244, 246),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 96, 59, 181),
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData(
+          color: kColorScheme.primaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+            foregroundColor: kColorScheme.onPrimaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onPrimaryContainer,
+            fontSize: 16,
+          ),
         ),
       ),
-      home: Expenses(),
+      home: const Expenses(),
     ),
   );
 }
