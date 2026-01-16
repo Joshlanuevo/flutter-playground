c6dae1f8603ca17c444ebef39a9a3d0463c17166
Ivan <lanuevo68@gmail.com>
2026-01-16 12:23:13 +0800

    updating the ui based on the available space

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -70,6 +71,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -91,9 +94,18 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
+      body: width < 600 ? Column(
         children: [
           Chart(expenses: _registeredExpenses),
+          Expanded(
+            child: mainContent,
+          ),
+        ],
+      ) : Row(
+        children: [
+          Expanded(
+            child: Chart(expenses: _registeredExpenses),
+          ),
           Expanded(
             child: mainContent,
           ),
