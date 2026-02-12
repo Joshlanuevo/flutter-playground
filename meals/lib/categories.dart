9475f548eedf854e212c7b52f12f0f5685dfdca8
Ivan <lanuevo68@gmail.com>
2026-02-13 00:58:58 +0800

    setup categories and use gridview

--- meals/lib/categories.dart
+++ meals/lib/categories.dart
@@ -2,10 +2,27 @@ import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Categories')),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          Text('Category 1'),
+          Text('Category 2'),
+          Text('Category 3'),
+          Text('Category 4'),
+          Text('Category 5'),
+          Text('Category 6'),
+        ],
+      ),
+    );
   }
 }
